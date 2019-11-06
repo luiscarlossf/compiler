2cdcb675a1a951521664b4b65aadd56af3b3627b
Luis <luiscarlos.sf@outlook.com>
2019-11-05 23:44:49 -0300

    First commit

---
+++ java-simple/JavaSimple.g4
@@ -0,0 +1,56 @@
+
+grammar JavaSimple;
+r : PROGRAM ; //NÃO ENTENDI PRA QUE SERVE ISSO!!!!!!!!!!!!
+
+fragment
+Letra : [a-z]+;
+fragment
+Inteiro: [0-9]+;
+fragment
+Float: Inteiro '.'?Inteiro*;
+
+DOISPONTOS: ':';
+VIRGULA: ',';
+CHAVEIN: '{';
+CHAVEOUT: '}';
+PARAIN: '(';
+PARAOUT: ')';
+PONTOVIRGULA: ';';
+
+//Palavras reservadas
+IF: 'if';
+PROGRAM: 'program';
+FUNC: 'func';
+RETURN: 'return';
+ELSE: 'else';
+PRINT: 'print';
+BLOCK: 'block';
+READ: 'read';
+FOR: 'for';
+ASSIGN: '=';
+CONST: 'const';
+TYPE: 'int'|'string'|'boolean'|'float';
+
+//Tipos
+STRING: '"'( ~('"'|'\n') )* '"'; //VERIFICAR ISSO, PRA VER SE TÁ PEGANDO A STRING CORRETAMENTE;
+BOOLEAN: ['TRUE''FALSE'];
+NUMBER: Inteiro|Float;
+
+//Operadores
+ NEG: '!';
+ LESS: '-';
+ PLUS: '+';
+ SUB: '-';
+ MULT: '*';
+ DIV: '/';
+ INC: '++';
+ DEC: '--';
+ RELOP: ['==''!=''>=''<=''>''<'];
+ 
+ 
+//Identificadores
+ ID : Letra (Letra|Inteiro)*; 
+ 
+ WS : [ \t\r\n]+ -> skip ; // skip spaces, tabs, newlines
+ 
+ 
---
+++ java-simple/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Lexer;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String fileName = "C:/Users/Luis Carlos/Documents/test.txt";
+        InputStream input = new FileInputStream(fileName);
+        ANTLRInputStream stream = new ANTLRInputStream(input);
+        //Lexico
+        JavaSimpleLexer lexer = new JavaSimpleLexer(stream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer); 
+        //Sintatico
+        JavaSimpleParser parser = new JavaSimpleParser(tokens);
+        ParseTree tree = parser.r();
+        System.out.println("\n"+ tokens.getTokens());
+        System.out.println(tree.toStringTree(parser));
+        System.out.println("end of program");
+    }
+
+}
